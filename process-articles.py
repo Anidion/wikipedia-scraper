5577fcfd7b33e6086e4d30ce7517186e065ebc6e
Ben Schmidt <ben@schmidtfam.us>
2024-09-01 14:06:10 -0600

    Regex kw search, renamed variables and functions to be more descriptive

--- process-articles.py
+++ process-articles.py
@@ -12,7 +12,7 @@ known_good_articles = [
     "SGI Dogfight",
 ]
 
-target_modes = ["network", "networked", "online"]
+query_keywords = ["network", "networked", "online"]
 
 debug_mode = True
 
@@ -21,39 +21,42 @@ def dbg(msg):
         print(msg)
 
 
-def process_article(text, filename="none"):
+def does_article_match(article_text):
+    # One of these headers is usually present at the end of the article
     article_end = re.search(
         r"==\s*References\s*==|==\s*Reception\s*==|==\s*Reviews\s*==|==\s*Sources\s*==|==\s*See also\s*==|==\s*External links\s*==",
-        text,
+        article_text,
     )
     if article_end is None:
-        article_end = len(text)
+        article_end = len(article_text)
     else:
         article_end = article_end.start()
 
-    for target in target_modes:
-        mode_start = text.rfind(f" {target} ", 0, article_end)
-        if mode_start != -1:
-            return True
+    # Find one matching keyword in the article
+    for kw in query_keywords:
+        found = re.search(r"\b" + kw + r"\b", article_text, re.IGNORECASE)
+        if found is not None:
+          return True
     return False
 
 
 def main():
     for root, _, files in os.walk("./out"):
-        for file in files:
-            with open(os.path.join(root, file), "r", encoding="utf-8") as article:
-                # First 7 lines are metadata, skip them
+        for file_name in files:
+            with open(os.path.join(root, file_name), "r", encoding="utf-8") as article:
+                # First 7 lines are metadata we added in dump-wikipedia.py, skip them
                 article_text = article.readlines()[7:]
                 article_text = "".join(article_text)
+                
+								# Skip short articles and redirects
                 if len(article_text) < 3:
                     continue
                 if article_text.find("#REDIRECT") != -1:
                     continue
 
-                mode_matches = process_article(article_text, file)
-                if mode_matches:
-                    dbg(f"Match found in {file}")
-                    with open(f"./matches/{file}", "w", encoding="utf-8") as out_file:
+                if does_article_match(article_text):
+                    dbg(f"Match found in {file_name}")
+                    with open(f"./matches/{file_name}", "w", encoding="utf-8") as out_file:
                         out_file.write(article_text)
 
 
